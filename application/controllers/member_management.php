001e41bbfcb8fbb61fceebaab98bbf1acbc8d7a6
norbiro000 <boomtingtong@hotmail.com>
2015-03-07 19:49:19 +0700

    add Member
    add member management

---
+++ application/controllers/member_management.php
@@ -0,0 +1,24 @@
+<?php 
+
+class Member_Management extends CI_Controller {
+	public function __construct() {
+	 	parent::__construct();
+	 }
+
+	public function index(){
+		
+	}
+
+	public function member_add(){
+		
+	}
+
+	public function member_edit(){
+		
+	}
+
+	public function member_delete(){
+		
+	}
+
+}?>
