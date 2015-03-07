8dd931be656d85cfe33eeaf4aa021d2c5c34d0fd
norbiro000 <boomtingtong@hotmail.com>
2015-03-07 21:03:07 +0700

    Add Member OK
    member_menagemant_add_save OK

--- application/controllers/member_management.php
+++ application/controllers/member_management.php
@@ -3,6 +3,8 @@
 class Member_Management extends CI_Controller {
 	public function __construct() {
 	 	parent::__construct();
+	 	$this->load->helper(array('form', 'url'));
+	 	$this->load->library('form_validation');
 	 }
 
 	public function index(){
@@ -10,7 +12,32 @@ class Member_Management extends CI_Controller {
 	}
 
 	public function member_add(){
-		
+		$this->load->view('member_management_add_view');
+	}
+
+	public function member_add_save(){
+		$this->form_validation->set_rules('studentid','studentid','required|min_length[10]|max_length[10]|is_unique[tb_member.member_studentID]');
+		$this->form_validation->set_rules('fname','fname','required');
+		$this->form_validation->set_rules('lname','lname','required');
+		$this->form_validation->set_rules('password','Password','required');
+		$this->form_validation->set_rules('conf-password','Password Confirmation','required|matches[password]');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('member_management_add_view');
+		}
+		else
+		{
+			$data = array('member_studentID' => $this->input->post('studentid'),
+				'member_username' => $this->input->post('studentid'),
+				'member_firstname' => $this->input->post('fname'),
+				'member_lastname' => $this->input->post('lname'),
+				'member_password' => $this->input->post('password'),
+				'member_type' => 'student'
+				);
+			$this->db->insert('tb_member',$data);
+			$this->load->view('member_management_add_view');
+		}
 	}
 
 	public function member_edit(){
---
+++ application/views/member_management_add_view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<div id="member_add_container">
+		<?php echo validation_errors() ?>
+		<?php echo form_open('member_management/member_add_save');?>
+			<label for="studentid">Student ID : </label><input type="text" name="studentid">
+			<label for="fname">Firstname : </label><input type="text" name="fname">
+			<label for="lname">Lastname : </label><input type="text" name="lname">
+			<label for="password">Password : </label><input type="password" name="password">
+			<label for="conf-password">Confirm-password : </label><input type="password" name="conf-password">
+			<input type="submit" value="Add">
+
+		<?php echo form_close()?>
+	</div>
+</body>
+</html>
